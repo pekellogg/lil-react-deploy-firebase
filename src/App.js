d8481e921e40af9e45219a2fc11134296e28ab0d
Peyton Kellogg <peytonkellogg@gmail.com>
2023-09-05 15:33:40 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useContext } from "react";
+import { Context } from "./context/FirestoreContext";
+import List from "./components/List";
+import "./App.css";
 
 function App() {
+  const { state } = useContext(Context);
+  const count = useMemo(() => {
+    return `you have ${state.items.length} image${
+      state.items.length > 1 ? "s" : ""
+    }`;
+  }, [state.items]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center">Gallery</h1>
+      {count}
+      <List items={state.items} />
+    </>
   );
 }
-
 export default App;
---
+++ src/images/Gallery.js
@@ -0,0 +1,25 @@
+import image1 from "./1.jpeg";
+import image2 from "./2.jpeg";
+import image3 from "./3.jpeg";
+import image4 from "./4.jpeg";
+import image5 from "./5.jpeg";
+import image6 from "./6.jpeg";
+import image7 from "./7.jpeg";
+import image8 from "./8.jpeg";
+import image9 from "./9.jpeg";
+import image10 from "./10.jpeg";
+
+const Gallery = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+];
+
+export default Gallery;
